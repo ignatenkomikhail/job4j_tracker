a82682f74911f657250260436abe242284ec061d
ignatenkomikhail <igna@inbox.ru>
2020-05-21 06:57:05 +0300

    5. Tracker - хранилище [396#284700]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,29 +1,58 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(generateId());
-        items[size++] = item;
+        item.setId(this.generateId());
+        this.items[this.size++] = item;
         return item;
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rnd = new Random();
+        return String.valueOf(rnd.nextLong() + System.currentTimeMillis());
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int index = 0; index < this.size; index++) {
+            Item item = this.items[index];
             if (item.getId().equals(id)) {
                 rsl = item;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] items = new Item[this.size];
+        int index = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (key.equals(this.items[i].getName())) {
+                items[index] = this.items[i];
+                index++;
+            }
+        }
+        items = Arrays.copyOf(items, index);
+        return items;
+    }
+
+    public Item[] findAll() {
+        Item[] items = new Item[this.size];
+        int index = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.items[i] != null) {
+                items[index] = this.items[i];
+                index++;
+            }
+        }
+        items = Arrays.copyOf(items, index);
+        return items;
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,33 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+    @Test
+    public void whenTrackerFindByName() {
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[2];
+        tracker.add(new Item("test1"));
+        Item item1 = new Item("test2");
+        tracker.add(item1);
+        items[0] = item1;
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        items[1] = item2;
+        tracker.add(new Item("test3"));
+        assertThat(items, is(tracker.findByName("test2")));
+    }
+    @Test
+    public void whenTrackerFindAll() {
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[3];
+        Item item1 = new Item("test1");
+        tracker.add(item1);
+        items[0] = item1;
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        items[1] = item2;
+        Item item3 = new Item("test3");
+        tracker.add(item3);
+        items[2] = item3;
+        assertThat(items, is(tracker.findAll()));
+    }
 }
